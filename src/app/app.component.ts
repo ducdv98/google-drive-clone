52c0a5939da1ce95b175f433374ca34cc359a6c6
ducdv98 <ducdv092@gmail.com>
2021-06-21 00:34:03 +0700

    Add favicon and page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'file-manager';
+  titleSuffix = 'Google Drive';
+
+  constructor(private titleService: Title,
+              private route: ActivatedRoute,
+              private router: Router) {
+    const appTitle = this.titleService.getTitle();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let child = this.route.firstChild;
+        while (child?.firstChild) {
+          child = child.firstChild;
+        }
+        if (child?.snapshot.data.title) {
+          return child.snapshot.data.title;
+        }
+        return appTitle;
+      })
+    ).subscribe((ttl: string) => {
+      this.titleService.setTitle(`${ttl} - ${this.titleSuffix}`);
+    });
+  }
 }
